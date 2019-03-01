a0dbd4090133bc21ee5e82b88b51b0505b2bf865
IlyesMohamedIlyes <45718480+IlyesMohamedIlyes@users.noreply.github.com>
2019-03-01 23:16:19 +0100

    Add files via upload

---
+++ TraitementImageProject/TraitementImageProject/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace TraitementImageProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button_Charger = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.label_DisplayValues = new System.Windows.Forms.Label();
+            this.textBox_DisplayValues = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(487, 54);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(253, 265);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button_Charger
+            // 
+            this.button_Charger.Location = new System.Drawing.Point(366, 171);
+            this.button_Charger.Name = "button_Charger";
+            this.button_Charger.Size = new System.Drawing.Size(75, 23);
+            this.button_Charger.TabIndex = 1;
+            this.button_Charger.Text = "CHARGER";
+            this.button_Charger.UseVisualStyleBackColor = true;
+            this.button_Charger.Click += new System.EventHandler(this.button_Charger_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.InitialImage")));
+            this.pictureBox2.Location = new System.Drawing.Point(52, 54);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(253, 265);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // label_DisplayValues
+            // 
+            this.label_DisplayValues.AutoSize = true;
+            this.label_DisplayValues.Location = new System.Drawing.Point(117, 343);
+            this.label_DisplayValues.Name = "label_DisplayValues";
+            this.label_DisplayValues.Size = new System.Drawing.Size(172, 13);
+            this.label_DisplayValues.TabIndex = 4;
+            this.label_DisplayValues.Text = "Les valeurs du Histogramme sont : ";
+            // 
+            // textBox_DisplayValues
+            // 
+            this.textBox_DisplayValues.Location = new System.Drawing.Point(290, 340);
+            this.textBox_DisplayValues.Multiline = true;
+            this.textBox_DisplayValues.Name = "textBox_DisplayValues";
+            this.textBox_DisplayValues.Size = new System.Drawing.Size(459, 98);
+            this.textBox_DisplayValues.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox_DisplayValues);
+            this.Controls.Add(this.label_DisplayValues);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button_Charger);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button_Charger;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.Label label_DisplayValues;
+        private System.Windows.Forms.TextBox textBox_DisplayValues;
+    }
+}
+
---
+++ TraitementImageProject/TraitementImageProject/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TraitementImageProject
+{
+    public partial class Form1 : Form
+    {
+        public string F
+        {
+            get { return "HHH"; }
+        }
+
+        public static Form1 Instance
+        {
+            get
+            {
+                if (Instance == null)
+                    Instance = new Form1();
+                return Instance;
+            }
+            set
+            {
+                
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Charger_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+
+            Bitmap bmap = new Bitmap(openFileDialog1.FileName);
+            pictureBox1.Image = bmap;
+
+            Bitmap bmapGris = convertirEnNiveauDeGris(bmap);
+            pictureBox2.Image = bmapGris;
+            calculHistogramme(bmapGris);
+        }
+
+        private Bitmap convertirEnNiveauDeGris(Bitmap imageBitmap)
+        {
+            Color c;
+            var imageData = new int[imageBitmap.Width, imageBitmap.Height];
+            Bitmap bitmapToreturn = new Bitmap(imageBitmap.Width, imageBitmap.Height);
+
+            int minNG = 255, maxNG = 0;
+
+            for(int i = 0; i < imageBitmap.Width; i++)
+            {
+                for(int j = 0; j < imageBitmap.Height; j++)
+                {
+                    c = imageBitmap.GetPixel(i, j);
+                    byte gray = (byte)(Math.Min(.299 * c.R + .587 * c.G + .114 * c.B, 255));
+
+                    bitmapToreturn.SetPixel(i, j, Color.FromArgb(gray, gray, gray));
+                    imageData[i, j] = gray;
+
+                    if (gray < minNG) minNG = gray;
+                    if (gray > maxNG) maxNG = gray;
+
+                }
+            }
+
+            return bitmapToreturn;
+        }
+
+        private void calculHistogramme(Bitmap imageBitmapEnGris)
+        {
+            Color c;
+            int[] histogramme = new int[256];
+
+            for(int i = 0; i < 256; i++)
+            {
+                histogramme[i] = 0;
+            }
+
+            for (int w = 0; w < imageBitmapEnGris.Width; w++)
+            {
+                for (int h = 0; h < imageBitmapEnGris.Height; h++)
+                {
+                    c = imageBitmapEnGris.GetPixel(w, h);
+                    histogramme[(byte)c.R]++;
+                }
+            }
+
+            foreach(int g in histogramme)
+            {
+                textBox_DisplayValues.Text += '-' + g;
+            }
+            //return histogramme;
+        }
+    }
+}
