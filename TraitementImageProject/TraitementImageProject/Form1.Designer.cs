d8e45f2816ffdec2790304d7dabfb16caa13cc89
IlyesMohamedIlyes <45718480+IlyesMohamedIlyes@users.noreply.github.com>
2019-03-06 00:08:06 +0100

    Update Form1.Designer.cs
    Ajout de : 
     * pictureBox3 qui represente l'histogramme
     * button_ShowHisto qui en cliquant affichera l'histogramme
    
    Modification des positions de  :
     * TOUT OUTIL EXISTANT SUR LA FORM1

--- TraitementImageProject/TraitementImageProject/Form1.Designer.cs
+++ TraitementImageProject/TraitementImageProject/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace TraitementImageProject
+namespace TraitementImageProject
 {
     partial class Form1
     {
@@ -33,20 +33,20 @@
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.button_Charger = new System.Windows.Forms.Button();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-            this.label_DisplayValues = new System.Windows.Forms.Label();
-            this.textBox_DisplayValues = new System.Windows.Forms.TextBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.button_ShowHisto = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
             this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
-            this.pictureBox1.Location = new System.Drawing.Point(487, 54);
+            this.pictureBox1.Location = new System.Drawing.Point(12, 25);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(253, 265);
+            this.pictureBox1.Size = new System.Drawing.Size(156, 156);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
@@ -57,7 +57,7 @@
             // 
             // button_Charger
             // 
-            this.button_Charger.Location = new System.Drawing.Point(366, 171);
+            this.button_Charger.Location = new System.Drawing.Point(48, 187);
             this.button_Charger.Name = "button_Charger";
             this.button_Charger.Size = new System.Drawing.Size(75, 23);
             this.button_Charger.TabIndex = 1;
@@ -69,56 +69,51 @@
             // 
             this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.pictureBox2.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.InitialImage")));
-            this.pictureBox2.Location = new System.Drawing.Point(52, 54);
+            this.pictureBox2.Location = new System.Drawing.Point(12, 216);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(253, 265);
+            this.pictureBox2.Size = new System.Drawing.Size(156, 156);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 2;
             this.pictureBox2.TabStop = false;
             // 
-            // menuStrip1
+            // pictureBox3
             // 
-            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
-            this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
-            this.menuStrip1.TabIndex = 3;
-            this.menuStrip1.Text = "menuStrip1";
+            this.pictureBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox3.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox3.InitialImage")));
+            this.pictureBox3.Location = new System.Drawing.Point(194, 65);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(594, 373);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 6;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox3_Paint);
             // 
-            // label_DisplayValues
+            // button_ShowHisto
             // 
-            this.label_DisplayValues.AutoSize = true;
-            this.label_DisplayValues.Location = new System.Drawing.Point(117, 343);
-            this.label_DisplayValues.Name = "label_DisplayValues";
-            this.label_DisplayValues.Size = new System.Drawing.Size(172, 13);
-            this.label_DisplayValues.TabIndex = 4;
-            this.label_DisplayValues.Text = "Les valeurs du Histogramme sont : ";
-            // 
-            // textBox_DisplayValues
-            // 
-            this.textBox_DisplayValues.Location = new System.Drawing.Point(290, 340);
-            this.textBox_DisplayValues.Multiline = true;
-            this.textBox_DisplayValues.Name = "textBox_DisplayValues";
-            this.textBox_DisplayValues.Size = new System.Drawing.Size(459, 98);
-            this.textBox_DisplayValues.TabIndex = 5;
+            this.button_ShowHisto.Location = new System.Drawing.Point(434, 38);
+            this.button_ShowHisto.Name = "button_ShowHisto";
+            this.button_ShowHisto.Size = new System.Drawing.Size(125, 21);
+            this.button_ShowHisto.TabIndex = 7;
+            this.button_ShowHisto.Text = "Afficher Histogramme";
+            this.button_ShowHisto.UseVisualStyleBackColor = true;
+            this.button_ShowHisto.Click += new System.EventHandler(this.button_ShowHisto_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.textBox_DisplayValues);
-            this.Controls.Add(this.label_DisplayValues);
+            this.Controls.Add(this.button_ShowHisto);
+            this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.button_Charger);
             this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.menuStrip1);
-            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -128,9 +123,8 @@
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.Button button_Charger;
         private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.MenuStrip menuStrip1;
-        private System.Windows.Forms.Label label_DisplayValues;
-        private System.Windows.Forms.TextBox textBox_DisplayValues;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button button_ShowHisto;
     }
 }
 
